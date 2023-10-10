4d8b95d1f6a0e39a994975af01e75210e99ebc75
trentiX <130845163+trentiX@users.noreply.github.com>
2023-10-10 23:55:28 +0600

    Final Commit

--- Wordle/Assets/Scripts/Board.cs
+++ Wordle/Assets/Scripts/Board.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Board : MonoBehaviour
 {
@@ -13,12 +15,61 @@ public class Board : MonoBehaviour
 
     private Row[] rows;
 
+    private string[] solutions;
+    private string[] validWords;
+    private string word;
+
     private int rowIndex;
     private int columnIndex;
+
+    [Header("States")]
+    public Tile.State emptyState;
+    public Tile.State occupiedState;
+    public Tile.State correctState;
+    public Tile.State wrongSpotState;
+    public Tile.State incorrectState;
+
+    [Header("UI")]
+    public TextMeshProUGUI invalidWordText;
+    public GameObject newWordButton;
+    public GameObject tryAgainButton;
 
     private void Awake()
     {
         rows = GetComponentsInChildren<Row>();
+    }
+    private void Start()
+    {
+        LoadData();
+        SetRandomWord();
+    }
+
+    public void NewGame()
+    {
+        ClearBoard();
+        SetRandomWord();
+
+        enabled = true;
+    }
+
+    public void TryAgain()
+    {
+        ClearBoard();
+
+        enabled = true;
+    }
+    private void LoadData()
+    {
+        TextAsset textFile = Resources.Load("official_wordle_all") as TextAsset;
+        validWords = textFile.text.Split('\n');
+
+        textFile = Resources.Load("official_wordle_common") as TextAsset;
+        solutions = textFile.text.Split('\n');
+    }
+    private void SetRandomWord()
+    {
+        word = solutions[Random.Range(0 , solutions.Length)];
+        word = word.ToLower().Trim();
     }
     private void Update()
     {
@@ -27,7 +78,11 @@ public class Board : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Backspace))
         {
             columnIndex = Mathf.Max(columnIndex - 1, 0);
+
             currentRow.tiles[columnIndex].SetLetter('\0');
+            currentRow.tiles[columnIndex].SetState(emptyState);
+
+            invalidWordText.gameObject.SetActive(false);
         }
         else if (columnIndex >= currentRow.tiles.Length)
         {
@@ -43,6 +98,7 @@ public class Board : MonoBehaviour
                 if (Input.GetKeyDown(SUPPORTED_KEYS[i]))
                 {
                     currentRow.tiles[columnIndex].SetLetter((char)SUPPORTED_KEYS[i]);
+                    currentRow.tiles[columnIndex].SetState(occupiedState);
                     columnIndex++;
                     break;
                 }
@@ -52,6 +108,116 @@ public class Board : MonoBehaviour
 
     private void SubmitRow(Row row)
     {
-        
+        if (!IsValidWord(row.word))
+        {
+            invalidWordText.gameObject.SetActive(true);
+            return;
+        }
+        string remaining = word;
+
+        for(int i = 0; i < row.tiles.Length; i++ )
+        {
+            Tile tile = row.tiles[i];
+
+            if (tile.letter == word[i])
+            {
+                tile.SetState(correctState);
+
+                remaining = remaining.Remove(i, 1);
+                remaining = remaining.Insert(i, " ");
+            }
+            else if (!word.Contains(tile.letter))
+            {
+                tile.SetState(incorrectState);
+            }
+        }
+
+        for (int i = 0; i < row.tiles.Length; i++)
+        {
+            Tile tile = row.tiles[i];
+
+            if (tile.state != correctState && tile.state != incorrectState)
+            {
+                if (remaining.Contains(tile.letter))
+                {
+                    tile.SetState(wrongSpotState);
+
+                    int index = remaining.IndexOf(tile.letter);
+                    remaining = remaining.Remove(index, 1);
+                    remaining = remaining.Insert(index, " ");
+                }
+                else
+                {
+                    tile.SetState(incorrectState);
+                }
+            }
+        }
+
+        if (HasWon(row))
+        {
+            enabled = false;
+        }
+
+        rowIndex++;
+        columnIndex = 0;
+
+        if (rowIndex >= rows.Length)
+        {
+            enabled = false;
+        }
+    }
+
+    private void ClearBoard()
+    {
+        for (int row = 0; row < rows.Length; row++)
+        {
+            for (int col = 0; col < rows[row].tiles.Length; col++)
+            {
+                rows[row].tiles[col].SetLetter('\0');
+                rows[row].tiles[col].SetState(emptyState);
+
+            }
+        }
+
+        rowIndex = 0;
+        columnIndex = 0; 
+    }
+
+    private bool IsValidWord(string word)
+    {
+        for (int i = 0; i < validWords.Length; i++)
+        {
+            if (validWords[i] == word)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private bool HasWon(Row row)
+    {
+        for (int i = 0; i < row.tiles.Length; i++)
+        {
+            if (row.tiles[i].state != correctState)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void OnEnable()
+    {
+        tryAgainButton.gameObject.SetActive(false);
+        newWordButton.gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        tryAgainButton.gameObject.SetActive(true);
+        newWordButton.gameObject.SetActive(true);
     }
 }
--- Wordle/Assets/Scripts/Tile.cs
+++ Wordle/Assets/Scripts/Tile.cs
@@ -4,18 +4,36 @@ using UnityEngine.UI;
 
 public class Tile : MonoBehaviour
 {
-    private TextMeshProUGUI text;
+    [System.Serializable]
+    public class State
+    {
+        public Color fillColor;
+        public Color outlineColor;
+    }
 
     public char letter { get; private set; }
+    public State state { get; private set; }
+
+    private TextMeshProUGUI text;
+    private Image fill;
+    private Outline outline;
 
     private void Awake()
     {
         text = GetComponentInChildren<TextMeshProUGUI>();
+        fill = GetComponent<Image>();
+        outline = GetComponent<Outline>();
     }
 
     public void SetLetter(char letter)
     {
         this.letter = letter;
         text.text = letter.ToString();
+    }
+    public void SetState(State state)
+    {
+        this.state = state;
+        fill.color = state.fillColor;
+        outline.effectColor = state.outlineColor;
     }
 }
